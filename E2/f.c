426cf83a77ccaf0b8685a12b34e40e689873973f
pzhwuhu <1434546761@qq.com>
2025-02-03 21:48:49 +0800

    fix bug in f.c

--- E2/f.c
+++ E2/f.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
+int price[3000001], visited[3000001];
 
 int main() {
   int n, m;
-  int price[3000001], visited[3000001];
   int max, max_i;
   scanf("%d%d", &n, &m);
   n++;
   m++;
   for (int i = 0; i < n; i++) {
     scanf("%d", &price[i]);
-    visited[i]=0;
   }
   for (int i = 0; i < m; i++) {
-    max = 0;
-    for (int j = 0; i < n; j++) {
+    max =  -1, max_i = 0;
+    for (int j = 0; j < n; j++) {
       if (max < price[j] && visited[j] == 0) {
         max = price[j];
         max_i = j;
@@ -21,7 +20,13 @@ int main() {
     }
     if (price[max_i] < 6)
       break;
-    
-
+    visited[max_i] = 1;
+    price[max_i] = 6;
   }
+
+  long long sum = 0;
+  for (int i = 0; i < n; i++) {
+    sum += price[i];
+  }
+  printf("%lld",sum);
 }
